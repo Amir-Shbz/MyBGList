b201e7f4d3cdde85b49f547da1bb004d11d60dfe
Ashnamth <ashnamth@gmail.com>
2025-03-16 11:42:13 -0700

    gRPC Finall

--- MyBGList/Controllers/GrpcController.cs
+++ MyBGList/Controllers/GrpcController.cs
@@ -1,6 +1,7 @@
-﻿using Grpc.Net.Client;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Grpc.Net.Client;
 using MyBGList.gRPC;
+using Grpc.Core;
 
 namespace MyBGList.Controllers
 {
@@ -11,10 +12,35 @@ namespace MyBGList.Controllers
         [HttpGet("{id}")]
         public async Task<BoardGameResponse> GetBoardGame(int id)
         {
-            using var channel = GrpcChannel.ForAddress("https://localhost:40443");
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:44350", 
+                                                        new GrpcChannelOptions { HttpHandler = handler });
             var client = new gRPC.Grpc.GrpcClient(channel);
             var response = await client.GetBoardGameAsync(
                                 new BoardGameRequest { Id = id });
+            return response;
+        }
+
+        [HttpPost]
+        public async Task<BoardGameResponse> UpdateBoardGame(string token, int id, string name)
+        {
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            var headers = new Metadata(); 
+            headers.Add("Authorization", $"Bearer {token}"); 
+            using var channel = GrpcChannel.ForAddress("https://localhost:44350", new GrpcChannelOptions { HttpHandler = handler });
+            var client = new gRPC.Grpc.GrpcClient(channel);
+            var response = await client.UpdateBoardGameAsync(
+                                new UpdateBoardGameRequest {
+                                    Id = id,
+                                    Name = name
+                                },
+                                headers);
             return response;
         }
     }
--- MyBGList/obj/Debug/net9.0/MyBGList.AssemblyInfo.cs
+++ MyBGList/obj/Debug/net9.0/MyBGList.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBGList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590eaea9bd0e43f555e15694b62a551b05d0fb6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a3da027689c77d3f960e31d71299b274c34ed8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBGList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBGList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
